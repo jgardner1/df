79720cb567a79b62c928c2cda6ea9835a34faf16
Jonathan Gardner <jgardner@socialcodeinc.com>
2013-01-16 11:18:18 -0800

    Shows some text on a pad for 5 seconds

--- df.py
+++ df.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 
 import curses
+import time
 
 def main():
     curses.wrapper(start)
 
 def start(stdscr):
-    pass
+    pad = curses.newpad(100,100)
+
+    for y in range(0,100):
+        for x in range(0,100):
+            try: pad.addch(y,x,ord('a')+(x*x+y*y) % 26)
+            except curses.error: pass
+
+    pad.refresh(0,0, 5,5, 20, 75)
+    time.sleep(5)
 
 if __name__ == '__main__':
     main()
