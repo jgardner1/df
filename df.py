84d99bd888c5c0a089ec720b2ea4b317a1c583bc
Jonathan Gardner <jgardner@socialcodeinc.com>
2013-01-16 15:28:33 -0800

    You can move the map around. Not cleanly, but OK

--- df.py
+++ df.py
@@ -7,13 +7,26 @@ import time
 
 class QuitGame(Exception): pass
 
+
+class Map(object):
+
+    def __init__(self):
+        self.tiles = [
+            [1,1,1,1,],
+            [1,1,1,1,],
+            [1,1,1,1,],
+        ]
+
 def main():
     pygame.display.init()
+    pygame.font.init()
+
     pygame.display.set_caption("Dwarf Frontier")
 
+    font = pygame.font.Font(None, 32)
+
+
     size = (width, height) = (1280, 720)
-    speed = [2, 2]
-    black = 0, 0, 0
 
     screen = pygame.display.set_mode(size,
         pygame.RESIZABLE | pygame.DOUBLEBUF)
@@ -21,13 +34,19 @@ def main():
     ball = pygame.image.load("ball.gif")
     ballrect = ball.get_rect()
 
-    frames = 0
-    last_fps = time.time()
+    # The game clock. This will record how many ticks have passed as well as
+    # limit us to 60 fps
     clock = pygame.time.Clock()
+
+    # This event will show the FPS
     SHOW_FPS = pygame.USEREVENT + 1
     pygame.time.set_timer(SHOW_FPS, 1000) 
+
+    title = font.render("Hello world!", True, pygame.Color(0,0,255,255))
+
+    viewport = pygame.Rect(0,0,100,100)
+    viewport_velocity = [0,0]
     while True:
-        frames += 1
         clock.tick(60)
 
         for event in pygame.event.get():
@@ -39,18 +58,43 @@ def main():
                 if event.key == pygame.K_ESCAPE:
                     raise QuitGame()
 
+                if event.key == pygame.K_UP:
+                    viewport_velocity[1] += 10
+                if event.key == pygame.K_DOWN:
+                    viewport_velocity[1] += -10
+                if event.key == pygame.K_LEFT:
+                    viewport_velocity[0] += -10
+                if event.key == pygame.K_RIGHT:
+                    viewport_velocity[0] += 10
+
+            if event.type == pygame.KEYUP:
+
+                if event.key == pygame.K_UP:
+                    viewport_velocity[1] -= 10
+                if event.key == pygame.K_DOWN:
+                    viewport_velocity[1] -= -10
+                if event.key == pygame.K_LEFT:
+                    viewport_velocity[0] -= -10
+                if event.key == pygame.K_RIGHT:
+                    viewport_velocity[0] -= 10
+
             if event.type == SHOW_FPS:
-                print clock.get_fps()
+                screen.blit(
+                    font.render("%0.1f" % (clock.get_fps()),
+                        True,
+                        pygame.Color(255,0,0),
+                        pygame.Color(0,0,0)),
+                    (300,0))
 
-        screen.fill(black, ballrect)
+        #screen.fill((0,0,0), ballrect)
 
-        ballrect = ballrect.move(speed)
-        if ballrect.left < 0 or ballrect.right > width:
-            speed[0] = -speed[0]
-        if ballrect.top < 0 or ballrect.bottom > height:
-            speed[1] = -speed[1]
+        viewport.move_ip(*viewport_velocity)
 
-        screen.blit(ball, ballrect)
+        for i in range(4):
+            for j in range(4):
+                screen.blit(ball, (i*64+viewport.left,j*64+viewport.top))
+
+        screen.blit(title, (0,0))
 
         pygame.display.flip()
 
