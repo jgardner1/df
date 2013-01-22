55c21a073566e737778831e1355887d43b7c0afd
Jonathan Gardner <jgardner@socialcodeinc.com>
2013-01-21 17:56:25 -0800

    Giving up on 3d for now

--- df.pyglet.py
+++ df.pyglet.py
@@ -13,7 +13,10 @@ from pyglet.gl import *
 
 import random
 
-window = pyglet.window.Window()
+def vec(*args):
+    return (GLfloat * len(args))(*args)
+
+window = pyglet.window.Window(fullscreen=True)
 
 # Game mode: Capture mouse, fullscreen
 #window = pyglet.window.Window(fullscreen=True)
@@ -50,6 +53,11 @@ def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
         camera[1] -= dy/10.0
 
 @window.event
+def on_mouse_scroll(x, y, scroll_x, scroll_y):
+    camera[2] += scroll_y
+    camera[2] = max(10, min(100, camera[2]))
+
+@window.event
 def on_resize(width, height):
     glViewport(0, 0, width, height)
     glMatrixMode(GL_PROJECTION)
@@ -70,26 +78,70 @@ batch.add_indexed(width*height, GL_QUADS, None,
     sum([(width*y+x, width*y+x+1, width*(y+1)+x+1, width*(y+1)+x)
         for y in range(height-1)
         for x in range(width-1)], ()),
-    ('v3i', sum([(x,y,random.choice((0, 2)))
+    ('v3i', sum([(x,y,0)
         for y in range(height)
         for x in range(width)], ())),
+    ('n3i', sum([(0,0,random.choice((0,1))) for y in range(height) for x in range(width)], ())),
     ('c3B', sum([
         (i*255/width/height, i*255/width/height, random.randrange(0,255))
         for i in range(width*height)], ())))
 
+sphere = gluNewQuadric()
+
+texture = resource.texture(
+    'rough-cobblestones-1/brick-floor-tileable_COLOR.jpg')
+texture_disp = resource.texture(
+    'rough-cobblestones-1/brick-floor-tileable_DISP.jpg')
+texture_spec = resource.texture(
+    'rough-cobblestones-1/brick-floor-tileable_SPEC.jpg')
+glBindTexture(GL_TEXTURE_2D, texture.id)
+
+glEnable(GL_TEXTURE_CUBE_MAP)
+cubemap = GLuint()
+glGenTextures(1, byref(cubemap))
+cubemap = cubemap.value
+glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR ) #  GL_NEAREST)
+glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR ) #  GL_NEAREST)
+
+
 @window.event
 def on_draw():
-    glEnable(GL_DEPTH_TEST)
     glClearColor(0.0, 0.0, 0.0, 0.0)
-    glClearDepth(1.0)
+
+    # Enable depth tests
+    glEnable(GL_DEPTH_TEST)
     glDepthFunc(GL_LESS)
+    glClearDepth(1.0)
+
+    glEnable(GL_CULL_FACE)
+
+    # Smooth colors between vertexes
     glShadeModel(GL_SMOOTH)
-    
+    glPolygonMode(GL_FRONT, GL_FILL)
+
+    glEnable(GL_LIGHTING)
+    glEnable(GL_LIGHT0)
+    glEnable(GL_LIGHT1)
+    glDisable(GL_LIGHT2)
+    glLightfv(GL_LIGHT0, GL_DIFFUSE,  vec(1.0, 1.0, 1.0, 1.0))
+    glLightfv(GL_LIGHT0, GL_SPECULAR, vec(1.0, 1.0, 1.0, 1.0))
+    glLightfv(GL_LIGHT0, GL_AMBIENT,  vec(0.0, 0.0, 0.0, 1.0))
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
+    glLightfv(GL_LIGHT0, GL_POSITION, vec(10.0, 10.0, 10.0, 1.0))
     glRotatef(-30.0, 1, 0, 0)
     glTranslatef(*[-x for x in camera])
 
+    glMaterialfv(GL_FRONT, GL_SPECULAR, vec(1.0, 1.0, 1.0, 1.0))
+    glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, vec(0.5, 1.0, 0.5, 1.0))
+    glMaterialfv(GL_FRONT, GL_SHININESS, vec(20.0))
+    glMaterialfv(GL_FRONT, GL_EMISSION, vec(0.2, 0.0, 0.0, 1.0))
+
     batch.draw()
+    gluSphere(sphere, 1.0, 10, 10)
 
 pyglet.app.run()
