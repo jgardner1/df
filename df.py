5e9a44afcb4c901152f23ee269cf3581b0da78f2
Jonathan Gardner <jgardner@socialcodeinc.com>
2013-02-12 17:42:27 -0800

    Drawing grass tiles instead of water.

--- df.py
+++ df.py
@@ -31,7 +31,7 @@ class MapModel(object):
         self.depth = 1
 
         self.tiles = transpose([
-            [random.choice([0,1,2,3]) for _ in range(self.width)]
+            [random.choice([4,5,6,7]) for _ in range(self.width)]
             for _ in range(self.height)])
 
     def terrain(self,x,y):
