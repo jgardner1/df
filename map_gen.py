31766c8660614b3d4e5a74c4939b8213e52b7b60
Jonathan Gardner <jgardner@jonathangardner.net>
2013-02-26 15:56:22 -0800

    Random terrain.

--- map_gen.py
+++ map_gen.py
@@ -5,6 +5,19 @@ import random
 
 from map import Map
 
+class Vec2(object):
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __add__(self, Vec2):
+        return Vec2(self.x+Vec2.x, self.y+Vec2.y)
+
+    def __iadd__(self, Vec2):
+        self.x += Vec2.x
+        self.y += Vec2.y
+        return self
+
 tiles = {
     # Empty
     0:(0,),
@@ -19,30 +32,38 @@ tiles = {
 }
 
 def map_gen(width=100, height=100):
-    depth = 20
+    depth = 5
     m = Map(width, height, depth)
 
-    # 1/4 of the depth will be empty
-    m.terrain_array.extend([0
-        for x in range(width)
-        for y in range(height)])
+    # Zero everything out. Bottom layer is stone.
+    m.terrain_array.extend([0]*width*height*(depth-1))
+    m.terrain_array.extend([4]*width*height)
 
-    # Grass on level 1
-    m.terrain_array.extend([2
-        for x in range(width)
-        for y in range(height)])
+    def coord(x,y,z):
+        return x + width*(y + height*z)
+
+    def get(x,y,z):
+        return m.terrain_array[coord(x,y,z)]
+
+    def set(x,y,z, val):
+        m.terrain_array[coord(x,y,z)] = val
+
+    for j in xrange(100):
+        pos = Vec2(random.randrange(width), random.randrange(height))
+        for i in xrange(100):
+            if 0 <= pos.x < width and 0 <= pos.y < height:
+                set(pos.x, pos.y, depth-2, 3)
+            pos += random.choice((
+                Vec2(1,0),
+                Vec2(-1,0),
+                Vec2(0,1),
+                Vec2(0,-1)))
+
+        
 
 
-    m.terrain_array.extend([3
-        for x in range(width)
-        for y in range(height)])
 
-
-    for z in range(depth-2):
-        m.terrain_array.extend([4
-            for x in range(width)
-            for y in range(height)])
-
+    # Turn the terrain into tiles
     for terrain in m.terrain_array:
         m.tile_array.append(
             random.choice(tiles[terrain]))
